49c393beee969dd810a126daaae47dc93e61ced1
Sven Kellenberger <kellenberger@ubique.ch>
2020-11-03 11:37:57 +0100

    simple qr code generation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,49 @@
-import './styles/main.css'
+import './styles/main.scss'
 import qrcode from 'qrcode-generator'
+import _sodium from 'libsodium-wrappers'
 
-var typeNumber = 4;
-var errorCorrectionLevel = 'L';
-var qr = qrcode(typeNumber, errorCorrectionLevel);
-qr.addData('https://ubique.ch');
-qr.make();
-document.getElementById('qrcode').innerHTML = qr.createImgTag(8, 16);
+let sodiumLoaded = false;
+let sodium;
+(async () => {
+    await _sodium.ready;
+    sodiumLoaded = true;
+    sodium = _sodium;
+})();
+
+const qrTypeNumber = 8;
+const qrErrorCorrectionLevel = 'L';
+
+let generateKeys = () => {
+    if (!sodiumLoaded) {
+        console.log("not yet loaded");
+        return;
+    }
+    const { publicKey, privateKey } = sodium.crypto_sign_keypair();
+
+    let qr = qrcode(qrTypeNumber, qrErrorCorrectionLevel);
+    qr.addData(`https://ubique.ch#${sodium.to_hex(privateKey)}`);
+    qr.make();
+    document.getElementById('private-key').innerHTML = qr.createSvgTag(10, 0);
+
+    qr = qrcode(qrTypeNumber, qrErrorCorrectionLevel);
+    qr.addData(`https://ubique.ch#${sodium.to_hex(publicKey)}`);
+    qr.make();
+    document.getElementById('public-key').innerHTML = qr.createSvgTag(10, 0);
+
+    const wrappers = document.getElementsByClassName("key-wrapper");
+    console.log(wrappers.length);
+    for(let i=0; i < wrappers.length; i++) wrappers[i].style.display = "initial";
+};
+
+let ready = (fn) => {
+    if (document.readyState != 'loading') {
+        fn();
+    } else {
+        document.addEventListener('DOMContentLoaded', fn);
+    }
+}
+
+ready(() => {
+    document.getElementById('generate-btn').onclick = () => { generateKeys() };
+})
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,41 @@
 const path = require('path');
 module.exports = {
- "mode": "none",
- "entry": "./src/index.js",
- "output": {
-   "path": __dirname + '/dist',
-   "filename": "bundle.js"
- },
-devServer: {
-   contentBase: path.join(__dirname, 'dist')
- },
- "module": {
-   "rules": [
-     {
-       "test": /\.css$/,
-       "use": [
-        "style-loader",
-         "css-loader"
-       ]
-     },
-     {
-       "test": /\.js$/,
-       "exclude": /node_modules/,
-       "use": {
-         "loader": "babel-loader",
-         "options": {
-           "presets": [
-             "@babel/preset-env",
-           ]
-         }
-       }
-     },	   
-   ]
- }	
+  "mode": "none",
+  "entry": ['babel-polyfill', "./src/index.js"],
+  "output": {
+    "path": __dirname + '/dist',
+    "filename": "bundle.js"
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist')
+  },
+  "module": {
+    "rules": [
+      {
+        "test": /\.scss$/,
+        "use": [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        "test": /\.js$/,
+        "exclude": /node_modules/,
+        "use": {
+          "loader": "babel-loader",
+          "options": {
+            "presets": [
+              "@babel/preset-env",
+            ]
+          }
+        }
+      },
+    ]
+  },
+  resolve: {
+    fallback: {
+      path: require.resolve("path-browserify")
+    }
+  }
 }
